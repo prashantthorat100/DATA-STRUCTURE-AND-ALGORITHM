bc0ab2ca60458a048de701f90fc375c7a76ac811
Prashant Thorat <prashant.workemail0@gmail.com>
2026-07-16 22:10:03 +0530

    Initial Commit

---
+++ ArrayCC.java
@@ -0,0 +1,22 @@
+
+
+public class ArrayCC{
+    public static void main(String[] args) {
+        // int marks[] = new int[50];
+        // // marks = {98,93,94,87,85};
+        // try(Scanner sc = new Scanner(System.in)){
+        //     marks[0] = sc.nextInt();
+        //     marks[1] = sc.nextInt();
+        //     marks[2] = sc.nextInt();
+        //     // System.out.print(marks);
+
+        //     for(int i =0;i<=50;i++){
+        //         System.out.println(marks[i]);
+        //     }
+        // }
+
+        int num[] = {2,3,4,5,6,7};
+        System.out.print("Length of Array is: "+num.length);
+
+    }
+}
