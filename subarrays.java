c56ad94c35bde558ee9f6f7bc789a2fa9377e7ff
Prashant Thorat <prashant.workemail0@gmail.com>
2026-07-26 22:53:38 +0530

    Question added

--- subarrays.java
+++ subarrays.java
@@ -1,27 +1,36 @@
 public class subarrays {
     public static void subArrays(int arr[]){
         int tsa =0;
+        int max = Integer.MIN_VALUE;
+        
         for(int i=0;i<arr.length;i++){
 
             for(int j=i;j<arr.length;j++){
                 int sum =0;
+                
+                
                 for(int k=i;k<=j;k++){
-                    System.out.print(arr[k]+ " ");
+                    // System.out.print(arr[k]+ " ");
                     sum = sum + arr[k];
+                    
+
                 }
-                System.out.print("    Sum of Subarray is : "+sum);
-                tsa++;
-                System.out.println();
+                System.out.println("Sum of Subarray is : "+sum);
+                if (max < sum) {
+                    max = sum;
+                }
+                
 
             }
             System.out.print("\n");
         }
         System.out.println("Total subArrays are :"+ tsa);
+        System.out.println("Max sum is "+ max);
     }
 
 
     public static void main(String[] args) {
-        int arr[] = {2,4,6,8,10};
+        int arr[] = {2,-4,6,-8,10,-1,3};
         subArrays(arr);
     }
 }
