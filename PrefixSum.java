1c66d10e24136d8fc23e5809cbcd3a4b3311b534
Prashant Thorat <prashant.workemail0@gmail.com>
2026-07-27 22:30:00 +0530

    Kadanes algo for not all negative numbers

--- PrefixSum.java
+++ PrefixSum.java
@@ -37,7 +37,7 @@ public class PrefixSum{
         printArray(copy);
     }
     public static void main(String[] args) {
-        int arr[] = {1,-2,6,-1,3};
+        int arr[] = {-2,-3,4,-1,-2,1,5,-3};
         prefixSumSubArray(arr);
     }
 }
