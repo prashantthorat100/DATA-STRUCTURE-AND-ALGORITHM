53f5062f7164d7fcab477d6af0ac966d417ee77c
Prashant Thorat <prashant.workemail0@gmail.com>
2026-08-16 00:31:23 +0530

    Matrix added

--- 2D_ARRAY/matrix.java
+++ 2D_ARRAY/matrix.java
@@ -1,11 +1,20 @@
-package 2D_ARRAY;
-
 import java.util.Scanner;
 
 public class matrix {
     public static void main(String[] args) {
         int arr[][] = new int[3][3];
+        int n=3,m=3;
         Scanner sc = new Scanner(System.in);
-        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
