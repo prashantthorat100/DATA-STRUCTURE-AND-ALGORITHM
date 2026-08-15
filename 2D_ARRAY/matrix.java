0049057b06df25ace37ede3b4f4b22c3043659b1
Prashant Thorat <prashant.workemail0@gmail.com>
2026-08-15 23:53:07 +0530

    2D array

---
+++ 2D_ARRAY/matrix.java
@@ -0,0 +1,11 @@
+package 2D_ARRAY;
+
+import java.util.Scanner;
+
+public class matrix {
+    public static void main(String[] args) {
+        int arr[][] = new int[3][3];
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
