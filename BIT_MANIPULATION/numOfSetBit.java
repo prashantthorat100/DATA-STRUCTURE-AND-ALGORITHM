ecd57483abf21bc916fa063eb746d3a8a2166963
Prashant Thorat <prashant.workemail0@gmail.com>
2026-09-13 00:40:18 +0530

    Fast Exponentitation Topic added

--- BIT_MANIPULATION/numOfSetBit.java
+++ BIT_MANIPULATION/numOfSetBit.java
@@ -1,3 +1,14 @@
 public class numOfSetBit {
-    
+    public static void main(String[] args) {
+        int num = 7;
+        int count =0;
+        while(num!=0){
+            if((num&1)!=0){
+                count++;
+            }
+            num = num>>1;
+            
+        }
+        System.out.print("Number of Set Bits are :"+count);
+    }
 }
