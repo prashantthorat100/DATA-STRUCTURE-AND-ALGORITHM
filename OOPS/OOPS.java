8f34bcb1a9eb097202dbd17ce362f8789218c186
Prashant Thorat <prashant.workemail0@gmail.com>
2026-09-16 21:48:42 +0530

    shallow Copy, and deep copy constructor topic added

--- OOPS/OOPS.java
+++ OOPS/OOPS.java
@@ -12,7 +12,42 @@ public class OOPS{
         myAcc.userName = "newUser";
         myAcc.setPassword("hello");
         System.out.println( myAcc.getPassword());
+
+        // Student s1 = new Student();
+        Student s2 = new Student();
+        Student s3 = new Student(22);
+        Student s4 = new Student("Pranav",27);
+        System.out.println(s4.name);
+        System.out.println(s2.name);
+        System.out.println(s3.age);
+        System.out.println(s4.rollNo + s4.name);
+
+        s2.name = "Sunny";
+        s2.age = 20;
+        s2.password = "movie";
        
+
+        s2.marks[0] = 89;
+        s2.marks[1] = 99;
+        s2.marks[2] = 79;
+        // Copy Constructor
+        Student s1 = new Student(s2);
+
+        System.out.println(s1.name);
+        System.out.println(s1.age);
+        System.out.println(s1.password);
+
+        s2.name = "dhiraj";
+        s2.marks[0] = 100;
+        System.out.println(s2.marks[0]);
+        System.out.println(s1.marks[0]);
+
+        
+
+
+
+        
+
 
     }
 }
@@ -43,4 +78,55 @@ class Pen{
     public int getTip(){
         return this.tip;
     }
+}
+
+class Student{
+    String name;
+    int age;
+    int rollNo;
+    String password;
+    int marks[] ;
+
+    // shallow copy Constructor
+    // Student(Student s2) {
+    //     marks = new int[3];
+    //     this.name = s2.name;
+    //     this.age = s2.age;
+    //     this.password = s2.password;
+    //     this.marks = s2.marks;
+
+    // }
+    // Deep Copy Constructor
+    Student(Student s2) {
+        marks = new int[3];
+        this.name = s2.name;
+        this.age = s2.age;
+        this.password = s2.password;
+        for(int i=0;i<marks.length;i++){
+            this.marks[i] = s2.marks[i];
+        }
+
+    }
+
+    Student() {
+        marks = new int[3];
+        System.out.println("Constructor is called");
+    }
+
+    public Student(String name) {
+        this.name = name;
+        marks = new int[3];
+    }
+    public Student(int age){
+        this.age = age;
+        marks = new int[3];
+    }
+    public Student(String name,int rollNo){
+        this.rollNo = rollNo;
+        this.name = name;
+        marks = new int[3];
+    }
+
+    
+
 }
